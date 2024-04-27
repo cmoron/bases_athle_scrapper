b00e40b6e9268af280b59fd1513b9dfbf6a76870
Cyril Moron <cyril.moron@gmail.com>
2024-04-27 22:53:45 +0200

    Refactor and update list_athletes.py for better readability and functionality
    - Updated the file docstring to use consistent capitalization for 'SQLite'.
    - Refactored CLUB_URL to a more readable format with additional comments.
    - Defined return types for function signatures to improve type clarity.
    - Updated comments and strings from French to English for consistency.
    - Simplified and optimized the athlete extraction and storage logic.
    - Moved database operations inside 'with' statements to ensure proper resource management.
    - Added detailed comments for functions to enhance code understanding and maintainability.
    - Corrected minor issues such as string manipulations and improved SQL execution strategies.

--- list_athletes.py
+++ list_athletes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-List athletes from a sqlite database file containing clubs id.
+List athletes from a SQLite database file containing clubs id.
 """
 
 import argparse
@@ -10,10 +10,13 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-CLUB_URL='https://bases.athle.fr/asp.net/liste.aspx?frmpostback=true&frmbase=resultats&frmmode=1&frmespace=0&frmsaison={year}&frmclub={club_id}&frmposition={page}'
+# URL of the club
+CLUB_URL = 'https://bases.athle.fr/asp.net/liste.aspx?frmpostback=true&frmbase=resultats&frmmode=1&frmespace=0&frmsaison={year}&frmclub={club_id}&frmposition={page}'
+
+# First year of the database
 FIRST_YEAR = 2004
 
-def generate_club_url(year: int, club_id: str, page: int = 0):
+def generate_club_url(year: int, club_id: str, page: int = 0) -> str:
     """
     Generate the URL for a club ID
     Args:
@@ -25,7 +28,7 @@ def generate_club_url(year: int, club_id: str, page: int = 0):
     """
     return CLUB_URL.format(year=year, club_id=club_id, page=page)
 
-def fetch_and_parse_html(url: str):
+def fetch_and_parse_html(url: str) -> BeautifulSoup:
     """
     Fetch and parse the HTML content of a URL
     Args:
@@ -43,20 +46,17 @@ def fetch_and_parse_html(url: str):
 
 def get_max_pages(soup: BeautifulSoup) -> int:
     """
-    Récupère le nombre de pages de clubs pour une année donnée
+    Get the number of club pages for a given year
     Args:
         soup (BeautifulSoup): The BeautifulSoup object
-
     Returns:
-        int: Nombre de pages de clubs
+        int: Number of club pages
     """
     max_pages = 0
     if soup:
         select_element = soup.find('select', class_='barSelect')
-
         if select_element:
             max_pages = len(select_element.find_all('option'))
-
     return max_pages
 
 def extract_athlete_data(athletes: dict, soup: BeautifulSoup) -> dict:
@@ -71,33 +71,41 @@ def extract_athlete_data(athletes: dict, soup: BeautifulSoup) -> dict:
     if soup:
         athlete_links = soup.find_all('a', href=lambda x: x and 'javascript:bddThrowAthlete' in x)
         for link in athlete_links:
-            id_athlete = link['href'].split(',')[1].strip()
+            id_athlete = link['href'].split(',')[1].strip("'")
             name_athlete = link.get_text(strip=True)
-            athletes[id_athlete] =  name_athlete
+            athletes[id_athlete] = name_athlete
     return athletes
 
-def create_athletes_table(database):
+def store_athletes(athletes: dict, cursor: sqlite3.Cursor):
+    """
+    Store the athletes in the database
+    Args:
+        athletes (dict): The athletes
+        cursor (sqlite3.Cursor): The cursor to execute SQL commands
+    """
+    cursor.executemany(
+        'INSERT OR IGNORE INTO athletes (id, name) VALUES (?, ?)',
+        list(athletes.items())
+    )
+
+def create_athletes_table(cursor: sqlite3.Cursor):
     """
     Create the athletes table
     Args:
-        database (str): The database file
+        cursor (sqlite3.Cursor): The cursor to execute SQL commands
     """
-    with sqlite3.connect(database) as conn:
-        cursor = conn.cursor()
-        cursor.execute('CREATE TABLE IF NOT EXISTS athletes (id TEXT PRIMARY KEY, name TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS athletes (id TEXT PRIMARY KEY, name TEXT)')
 
-def retrieve_clubs(database) -> dict:
+def retrieve_clubs(cursor: sqlite3.Cursor) -> dict:
     """
-    Retrieve clubs from the database
+    Retrieve the clubs from the database
     Args:
-        database (str): The database file
+        cursor (sqlite3.Cursor): The cursor to execute SQL commands
     Returns:
         dict: The clubs
     """
-    with sqlite3.connect(database) as conn:
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM clubs')
-        return dict(cursor.fetchall())
+    cursor.execute('SELECT * FROM clubs')
+    return dict(cursor.fetchall())
 
 def extract_athletes_from_club(year: int, club_id: str) -> dict:
     """
@@ -106,56 +114,45 @@ def extract_athletes_from_club(year: int, club_id: str) -> dict:
         year (int): The year
         club_id (str): The club ID
     Returns:
-        dict: The athletes dictionary
+        dict: The athletes
     """
     athletes = {}
     url = generate_club_url(year, club_id)
     soup = fetch_and_parse_html(url)
-    max_pages = get_max_pages(soup)
-    for page in range(max_pages):
-        paginate_url = generate_club_url(year, club_id, page)
-        paginate_soup = fetch_and_parse_html(paginate_url)
-        athletes = extract_athlete_data(athletes, paginate_soup)
+    if soup:
+        max_pages = get_max_pages(soup)
+        for page in range(max_pages):
+            paginate_url = generate_club_url(year, club_id, page)
+            paginate_soup = fetch_and_parse_html(paginate_url)
+            athletes.update(extract_athlete_data({}, paginate_soup))
     return athletes
-
-def store_athletes(athletes: dict, database: str):
-    """
-    Store the athletes in the database
-    Args:
-        athletes (dict): The athletes
-        database (str): The database file
-    """
-    with sqlite3.connect(database) as conn:
-        cursor = conn.cursor()
-        for athlete_id, name in athletes.items():
-            cursor.execute(
-                    'INSERT OR IGNORE INTO athletes (id, name) VALUES (?, ?)', (athlete_id, name))
 
 def main():
     """
     Main function
     """
-    parser = argparse.ArgumentParser(
-            description="List athletes from a sqlite database file containing clubs id.")
+    parser = argparse.ArgumentParser(description="List athletes from a SQLite database file containing clubs id.")
     parser.add_argument('database', type=str, help='Path to the SQLite3 clubs database file.')
     args = parser.parse_args()
     current_year = datetime.now().year
 
     try:
-        create_athletes_table(args.database)
-        clubs = retrieve_clubs(args.database)
-        nb_clubs = len(clubs)
-        cpt = 0
+        with sqlite3.connect(args.database) as conn:
+            cursor = conn.cursor()
+            create_athletes_table(cursor)
+            clubs = retrieve_clubs(cursor)
+            nb_clubs = len(clubs)
+            cpt = 0
 
-        #extract athletes from clubs
-        for club_id in clubs:
-            cpt += 1
-            for year in range(FIRST_YEAR, current_year + 1):
-                print(cpt, "/", nb_clubs, " - Processing club", clubs[club_id], "for year", year)
-                athletes = extract_athletes_from_club(year, club_id)
-                store_athletes(athletes, args.database)
+            for club_id in clubs:
+                cpt += 1
+                for year in range(FIRST_YEAR, current_year + 1):
+                    print(f"{cpt} / {nb_clubs} - Processing club {clubs[club_id]} for year {year}")
+                    athletes = extract_athletes_from_club(year, club_id)
+                    store_athletes(athletes, cursor)
+            conn.commit()
     except FileNotFoundError:
-        print(f"Error: File {args.csv_file} not found.", file=sys.stderr)
+        print(f"Error: File {args.database} not found.", file=sys.stderr)
     except requests.RequestException as e:
         print(f"Error: {e}", file=sys.stderr)
     except sqlite3.Error as e:
