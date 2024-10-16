737baa8863ae8c37948b293ee6fa7095122c3c69
Cyril Moron <cyril.moron@gmail.com>
2024-10-16 10:11:25 +0200

    Update the update data script to match bases athle season policy

--- update_database.sh
+++ update_database.sh
@@ -3,6 +3,14 @@
 # Définir les variables
 SCRIPTS_DIR="$(dirname "$(readlink -f "$0")")"
 CURRENT_YEAR="$(date +"%Y")"
+CURRENT_MONTH="$(date +"%m")"
+
+# Vérifier si on est après août (mois >= 9)
+if [ "$CURRENT_MONTH" -ge 9 ]; then
+    SEASON=$((CURRENT_YEAR + 1))
+else
+    SEASON="$CURRENT_YEAR"
+fi
 
 # Exécuter les scripts de mise à jour
 cd "${SCRIPTS_DIR}"
@@ -13,7 +21,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-./list_athletes.py --first-year "${CURRENT_YEAR}"
+./list_athletes.py --first-year "${SEASON}"
 if [ $? -ne 0 ]; then
     echo "Échec de l'exécution de list_athletes.py"
     exit 1
